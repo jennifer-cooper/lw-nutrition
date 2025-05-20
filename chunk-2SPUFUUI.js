ba67e8b1dba399b43d40545f8618526be49046f6
jennifer-cooper <jennifer.cooper@jisc.ac.uk>
2025-05-20 22:27:16 +0100

    Auto-generated commit

--- chunk-2SPUFUUI.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var o=[{path:"",title:"Home \u2013 Louise Wilkinson Nutrition",loadComponent:()=>import("./chunk-M3VW7TSU.js").then(e=>e.HomePage)}];export{o as homeRoutes};
