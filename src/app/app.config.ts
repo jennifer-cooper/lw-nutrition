4a2386aa78c147e8a9f3524b0ea4b63568e5a452
jennifer-cooper <jennifer.cooper@jisc.ac.uk>
2025-05-31 14:46:12 +0100

    lw fixes and some further tweaks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import {
   provideClientHydration,
   withEventReplay,
@@ -9,7 +9,10 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled'
+    })),
     provideClientHydration(withEventReplay()),
   ],
 };
