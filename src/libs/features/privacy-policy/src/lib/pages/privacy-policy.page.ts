6e53a61fab53b9bd14304fc07f5b04906bfe6f8e
Jennifer Cooper <jennifer.cooper25@gmail.com>
2025-07-12 21:53:16 +0100

    adding animation of dropdown to privacy page

--- src/libs/features/privacy-policy/src/lib/pages/privacy-policy.page.ts
+++ src/libs/features/privacy-policy/src/lib/pages/privacy-policy.page.ts
@@ -5,6 +5,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone: true,
   templateUrl: './privacy-policy-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [`
+    details summary::-webkit-details-marker {
+      display: none;
+    }
+    details > summary i {
+      transition: transform 0.3s ease;
+    }
+
+    details[open] > summary i {
+      transform: rotate(180deg);
+    }
+  `],
   imports: [],
 })
 export class PrivacyPolicyPage {}
